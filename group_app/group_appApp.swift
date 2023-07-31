748ca02e7ab9cf918c4a94aa741d74ea122b239e
emilym <emily.markova@gmail.com>
2023-07-31 13:09:27 -0700

    Initial Commit

---
+++ group_app/group_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  group_appApp.swift
+//  group_app
+//
+//  Created by Emily Markova on 7/31/23.
+//
+
+import SwiftUI
+
+@main
+struct group_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
